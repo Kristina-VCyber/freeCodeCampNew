34f54e33af6ea0d24059fb987c5a66c6039157e3
Kristina-VCyber <hatkova@inbox.ru>
2022-01-31 23:08:22 +0300

    Use Recursion to Create a Countdown

--- freeCodeCampNew.js
+++ freeCodeCampNew.js
@@ -201,16 +201,29 @@
 
 
 // Nesting For Loops
-function multiplyAll(arr) {
-  let product = 1;
-  // Only change code below this line
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
-      product = product * (arr[i][j]);
-    }
-  }
-  // Only change code above this line
-  return product;
-}
+// function multiplyAll(arr) {
+//   let product = 1;
+//   // Only change code below this line
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//       product = product * (arr[i][j]);
+//     }
+//   }
+//   // Only change code above this line
+//   return product;
+// }
+//
+// multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
 
-multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+
+// Use Recursion to Create a Countdown
+function countdown (n){
+  if (n < 1) {
+    return [];
+  } else {
+    const countDownArray = countdown(n-1);
+    countDownArray.unshift(n);
+    return countDownArray;
+
+  }
+}
