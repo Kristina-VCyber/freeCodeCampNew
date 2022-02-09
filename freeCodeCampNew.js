8fe6fad6f03688eaa465ea03a61ecd457d5b86a5
Kristina-VCyber <hatkova@inbox.ru>
2022-02-09 22:14:27 +0300

    Use Arrow Functions to Write Concise Anonymous Functions

--- freeCodeCampNew.js
+++ freeCodeCampNew.js
@@ -281,32 +281,35 @@ function countdown(n) {
 
 
 // Counting Cards
-let count = 0;
+// let count = 0;
+//
+// function cc(card) {
+//     // Only change code below this line
+//     switch (card) {
+//         case 2:
+//         case 3:
+//         case 4:
+//         case 5:
+//         case 6:
+//             count++;
+//             break;
+//         case 10:
+//         case "J":
+//         case "Q":
+//         case "K":
+//         case "A":
+//             count--;
+//             break;
+//     }
+//     if (count > 0) {
+//         return count + " Bet";
+//     } else {
+//         return count + " Hold";
+//     }
+//     // Only change code above this line
+// }
+//
+// cc(2); cc(3); cc(7); cc('K'); cc('A');
 
-function cc(card) {
-    // Only change code below this line
-    switch (card) {
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-        case 6:
-            count++;
-            break;
-        case 10:
-        case "J":
-        case "Q":
-        case "K":
-        case "A":
-            count--;
-            break;
-    }
-    if (count > 0) {
-        return count + " Bet";
-    } else {
-        return count + " Hold";
-    }
-    // Only change code above this line
-}
-
-cc(2); cc(3); cc(7); cc('K'); cc('A');
+// Use Arrow Functions to Write Concise Anonymous Functions
+const magic = () => new Date() ;
