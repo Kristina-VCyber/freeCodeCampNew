25f43a5f2ce530685d77bd0f3b68b8b6c4bde978
Kristina-VCyber <hatkova@inbox.ru>
2022-02-10 19:34:12 +0300

    Use the Spread Operator to Evaluate Arrays In-Place

--- freeCodeCampNew.js
+++ freeCodeCampNew.js
@@ -348,3 +348,13 @@ function freezeObj() {
     return MATH_CONSTANTS.PI;
 }
 const PI = freezeObj();
+
+
+// Use the Spread Operator to Evaluate Arrays In-Place
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  // Change this line
+
+console.log(arr2);
