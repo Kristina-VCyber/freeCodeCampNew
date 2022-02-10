f71297df254567792f281d0d877439fa4b2e29ab
Kristina-VCyber <hatkova@inbox.ru>
2022-02-10 22:48:30 +0300

    Use Destructuring Assignment with the Rest Parameter to Reassign Array Elements

--- freeCodeCampNew.js
+++ freeCodeCampNew.js
@@ -361,45 +361,55 @@
 
 // Use Destructuring Assignment to Extract Values from Objects
 
-const HIGH_TEMPERATURES = {
-    yesterday: 75,
-    today: 77,
-    tomorrow: 80
-};
+// const HIGH_TEMPERATURES = {
+//     yesterday: 75,
+//     today: 77,
+//     tomorrow: 80
+// };
+//
+// // Only change code below this line
+// const {today, tomorrow} = HIGH_TEMPERATURES;
+// // Only change code above this line
+//
+//
+// // Use Destructuring Assignment to Assign Variables from Objects
+//
+// const HIGH_TEMPERATURES = {
+//     yesterday: 75,
+//     today: 77,
+//     tomorrow: 80
+// };
+//
+// // Only change code below this line
+// const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;
+//
+//
+// // Only change code above this line
+//
+//
+// // Use Destructuring Assignment to Assign Variables from Nested Objects
+// const LOCAL_FORECAST = {
+//     yesterday: { low: 61, high: 75 },
+//     today: { low: 64, high: 77 },
+//     tomorrow: { low: 68, high: 80 }
+// };
+//
+// // Only change code below this line
+// const {today: {low:lowToday , high: highToday}} = LOCAL_FORECAST
+// // Only change code above this line
+//
+// // Use Destructuring Assignment to Assign Variables from Arrays
+//
+// let a = 8, b = 6;
+// // Only change code below this line
+// [a,b] = [b,a];
 
-// Only change code below this line
-const {today, tomorrow} = HIGH_TEMPERATURES;
-// Only change code above this line
-
-
-// Use Destructuring Assignment to Assign Variables from Objects
-
-const HIGH_TEMPERATURES = {
-    yesterday: 75,
-    today: 77,
-    tomorrow: 80
-};
-
-// Only change code below this line
-const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;
-
-
-// Only change code above this line
-
-
-// Use Destructuring Assignment to Assign Variables from Nested Objects
-const LOCAL_FORECAST = {
-    yesterday: { low: 61, high: 75 },
-    today: { low: 64, high: 77 },
-    tomorrow: { low: 68, high: 80 }
-};
-
-// Only change code below this line
-const {today: {low:lowToday , high: highToday}} = LOCAL_FORECAST
-// Only change code above this line
-
-// Use Destructuring Assignment to Assign Variables from Arrays
-
-let a = 8, b = 6;
-// Only change code below this line
-[a,b] = [b,a];
+// Use Destructuring Assignment with the Rest Parameter to Reassign Array Elements
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+    // Only change code below this line
+    const [a,b, ...arr] = list; // Change this line
+    // Only change code above this line
+    return arr;
+}
+const arr = removeFirstTwo(source);
