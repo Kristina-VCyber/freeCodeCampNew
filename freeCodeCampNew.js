1f034e0f45443f7bae397a86c774ac98f3ec8eef
Kristina-VCyber <hatkova@inbox.ru>
2022-02-10 20:03:15 +0300

    Use Destructuring Assignment to Assign Variables from Nested Objects

--- freeCodeCampNew.js
+++ freeCodeCampNew.js
@@ -385,3 +385,17 @@ const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;
 
 
 // Only change code above this line
+
+
+// Use Destructuring Assignment to Assign Variables from Nested Objects
+const LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 }
+};
+
+// Only change code below this line
+const {today: {low:lowToday , high: highToday}} = LOCAL_FORECAST
+
+
+// Only change code above this line
