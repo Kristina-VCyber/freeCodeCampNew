718afc4e525cf8c11d5bdbb523eae897e554564b
Kristina-VCyber <hatkova@inbox.ru>
2022-02-10 13:33:45 +0300

    Prevent Object Mutation

--- freeCodeCampNew.js
+++ freeCodeCampNew.js
@@ -329,3 +329,22 @@ const sum = (...args) => {
     return args.reduce((a, b) => a + b, 0);
 }
 // Only change code above this line
+
+
+// Prevent Object Mutation
+function freezeObj() {
+    const MATH_CONSTANTS = {
+        PI: 3.14
+    };
+    // Only change code below this line
+    Object.freeze(MATH_CONSTANTS);
+
+    // Only change code above this line
+    try {
+        MATH_CONSTANTS.PI = 99;
+    } catch(ex) {
+        console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
