07134f56e4c2d33907460c015fb06f196d1352e2
Kristina-VCyber <hatkova@inbox.ru>
2022-02-13 15:34:53 +0300

    Generate Random Whole Numbers within a Range

--- freeCodeCampNew.js
+++ freeCodeCampNew.js
@@ -425,3 +425,9 @@ function rangeOfNumbers(startNum, endNum) {
         return numbersArr;
     }
 }
+// Generate Random Whole Numbers within a Range
+function randomRange(myMin, myMax) {
+    // Only change code below this line
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+    // Only change code above this lin
+}
