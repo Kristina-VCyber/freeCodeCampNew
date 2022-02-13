3b3e10f30c42007c976e15e59c683144ed094207
Kristina-VCyber <hatkova@inbox.ru>
2022-02-13 21:22:26 +0300

    Create Strings using Template Literals

--- freeCodeCampNew.js
+++ freeCodeCampNew.js
@@ -458,3 +458,24 @@ const stats = {
 // Only change code below this line
 const half = ({ max, min }) => (max + min) / 2.0;
 // Only change code above this line
+
+
+// Create Strings using Template Literals
+
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+
+    // change code below this line
+    const failureItems = [];
+    for (let i = 0; i < arr.length; i++) {
+        failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
+    }
+    // change code above this line
+    return failureItems;
+}
+
+const failuresList = makeList(result.failure);
