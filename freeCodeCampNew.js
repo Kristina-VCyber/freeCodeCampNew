be2d2967041e7a3e980b75bef46fd3e6da232484
Kristina-VCyber <hatkova@inbox.ru>
2022-02-13 21:27:46 +0300

    Write Concise Object Literal Declarations Using Object Property Shorthand

--- freeCodeCampNew.js
+++ freeCodeCampNew.js
@@ -433,49 +433,58 @@
 // }
 
 // Profile Lookup
-function lookUpProfile(name, prop) {
-    for (let i = 0; i < contacts.length; i++) {
-        if (contacts[i].firstName === name) {
-            if (contacts[i].hasOwnProperty(prop)) {
-                return contacts[i][prop];
-            } else {
-                return "No such property";
-            }
-        }
-    }
-    return "No such contact";
-}
-// Use Destructuring Assignment to Pass an Object as a Function's Parameters
-const stats = {
-    max: 56.78,
-    standard_deviation: 4.34,
-    median: 34.54,
-    mode: 23.87,
-    min: -0.75,
-    average: 35.85
+// function lookUpProfile(name, prop) {
+//     for (let i = 0; i < contacts.length; i++) {
+//         if (contacts[i].firstName === name) {
+//             if (contacts[i].hasOwnProperty(prop)) {
+//                 return contacts[i][prop];
+//             } else {
+//                 return "No such property";
+//             }
+//         }
+//     }
+//     return "No such contact";
+// }
+// // Use Destructuring Assignment to Pass an Object as a Function's Parameters
+// const stats = {
+//     max: 56.78,
+//     standard_deviation: 4.34,
+//     median: 34.54,
+//     mode: 23.87,
+//     min: -0.75,
+//     average: 35.85
+// };
+//
+// // Only change code below this line
+// const half = ({ max, min }) => (max + min) / 2.0;
+// // Only change code above this line
+//
+//
+// // Create Strings using Template Literals
+//
+// const result = {
+//     success: ["max-length", "no-amd", "prefer-arrow-functions"],
+//     failure: ["no-var", "var-on-top", "linebreak"],
+//     skipped: ["no-extra-semi", "no-dup-keys"]
+// };
+// function makeList(arr) {
+//
+//     // change code below this line
+//     const failureItems = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
+//     }
+//     // change code above this line
+//     return failureItems;
+// }
+//
+// const failuresList = makeList(result.failure);
+
+
+// Write Concise Object Literal Declarations Using Object Property Shorthand
+
+const createPerson = (name, age, gender) => {
+    // Only change code below this line
+    return ({name, age, gender});
+    // Only change code above this line
 };
-
-// Only change code below this line
-const half = ({ max, min }) => (max + min) / 2.0;
-// Only change code above this line
-
-
-// Create Strings using Template Literals
-
-const result = {
-    success: ["max-length", "no-amd", "prefer-arrow-functions"],
-    failure: ["no-var", "var-on-top", "linebreak"],
-    skipped: ["no-extra-semi", "no-dup-keys"]
-};
-function makeList(arr) {
-
-    // change code below this line
-    const failureItems = [];
-    for (let i = 0; i < arr.length; i++) {
-        failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
-    }
-    // change code above this line
-    return failureItems;
-}
-
-const failuresList = makeList(result.failure);
