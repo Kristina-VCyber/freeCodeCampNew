e6470910c6d142ad4ac4faae53eb2280890b22bb
Kristina-VCyber <hatkova@inbox.ru>
2022-02-13 22:01:55 +0300

    Use getters and setters to Control Access to an Object

--- freeCodeCampNew.js
+++ freeCodeCampNew.js
@@ -516,3 +516,25 @@ class Vegetable {
 
 const carrot = new Vegetable('carrot');
 console.log(carrot.name); // Should display 'carrot'
+
+// Use getters and setters to Control Access to an Object
+// Only change code below this line
+class Thermostat {
+    constructor(fahrenheitTemp) {
+        this.fahrenheitTemp = fahrenheitTemp;
+    }
+
+    get temperature() {
+        return (5 / 9) * (this.fahrenheitTemp - 32);
+    }
+
+    set temperature(celsiusTemp) {
+        this.fahrenheitTemp = (celsiusTemp * 9.0) / 5 + 32;
+    }
+}
+// Only change code above this line
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
