003db031aa9f3922003cf2b1ca441d31c1f86b86
Kristina-VCyber <hatkova@inbox.ru>
2022-02-13 15:46:49 +0300

    Profile Lookup

--- freeCodeCampNew.js
+++ freeCodeCampNew.js
@@ -431,3 +431,17 @@ function randomRange(myMin, myMax) {
     return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
     // Only change code above this lin
 }
+
+// Profile Lookup
+function lookUpProfile(name, prop) {
+    for (let i = 0; i < contacts.length; i++) {
+        if (contacts[i].firstName === name) {
+            if (contacts[i].hasOwnProperty(prop)) {
+                return contacts[i][prop];
+            } else {
+                return "No such property";
+            }
+        }
+    }
+    return "No such contact";
+}
