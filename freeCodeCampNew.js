f03d22c6c8f927d11b75fd7d63c9b301bb5f6309
Kristina-VCyber <hatkova@inbox.ru>
2022-02-13 22:18:33 +0300

    Import a Default Export

--- freeCodeCampNew.js
+++ freeCodeCampNew.js
@@ -583,6 +583,12 @@
 
 // Create an Export Fallback with export default
 
-export default function subtract(x, y) {
-    return x - y;
-}
+// export default function subtract(x, y) {
+//     return x - y;
+// }
+
+// Import a Default Export
+import subtract from "./math_functions.js";
+// Only change code above this line
+
+subtract(7,4);
