1d95c178b388437f130ca43868b6ad49d7f1b9ca
Kristina-VCyber <hatkova@inbox.ru>
2022-02-13 22:12:11 +0300

    Reuse JavaScript Code Using import

--- freeCodeCampNew.js
+++ freeCodeCampNew.js
@@ -560,3 +560,13 @@ const lowercaseString = (string) => {
     return string.toLowerCase()
 }
 export {lowercaseString};
+
+
+
+// Reuse JavaScript Code Using import
+
+import {uppercaseString, lowercaseString} from "./string_functions.js" ;
+// Only change code above this line
+
+uppercaseString("hello");
+lowercaseString("WORLD!");
