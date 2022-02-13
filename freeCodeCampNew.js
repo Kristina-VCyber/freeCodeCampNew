64adbf9c28e2433a6b301ea100585115abf6d2d9
Kristina-VCyber <hatkova@inbox.ru>
2022-02-13 22:14:31 +0300

    Use * to Import Everything from a File

--- freeCodeCampNew.js
+++ freeCodeCampNew.js
@@ -552,21 +552,30 @@
 <!-- Only change code above this line -->
 
 // Use export to Share a Code Block
-const uppercaseString = (string) => {
-    return string.toUpperCase();
-}
-export {uppercaseString};
-const lowercaseString = (string) => {
-    return string.toLowerCase()
-}
-export {lowercaseString};
+// const uppercaseString = (string) => {
+//     return string.toUpperCase();
+// }
+// export {uppercaseString};
+// const lowercaseString = (string) => {
+//     return string.toLowerCase()
+// }
+// export {lowercaseString};
 
 
 
 // Reuse JavaScript Code Using import
 
-import {uppercaseString, lowercaseString} from "./string_functions.js" ;
+// import {uppercaseString, lowercaseString} from "./string_functions.js" ;
+// // Only change code above this line
+//
+// uppercaseString("hello");
+// lowercaseString("WORLD!");
+
+
+// Use * to Import Everything from a File
+
+import * as stringFunctions from "./string_functions.js"
 // Only change code above this line
 
-uppercaseString("hello");
-lowercaseString("WORLD!");
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
