4687e5bc9028e62ebdbf40810caeb05a89dff13b
Kristina-VCyber <hatkova@inbox.ru>
2022-02-15 22:33:24 +0300

    Handle a Fulfilled Promise with then

--- freeCodeCampNew.js
+++ freeCodeCampNew.js
@@ -599,13 +599,28 @@
 // });
 
 // Complete a Promise with resolve and reject
+// const makeServerRequest = new Promise((resolve, reject) => {
+//     // responseFromServer represents a response from a server
+//     let responseFromServer;
+//
+//     if(responseFromServer) {
+//         resolve ("We got the data");
+//     } else {
+//         reject ("Data not received");
+//     }
+// });
+
+// Handle a Fulfilled Promise with then
 const makeServerRequest = new Promise((resolve, reject) => {
-    // responseFromServer represents a response from a server
-    let responseFromServer;
+    // responseFromServer is set to true to represent a successful response from a server
+    let responseFromServer = true;
 
     if(responseFromServer) {
-        resolve ("We got the data");
+        resolve("We got the data");
     } else {
-        reject ("Data not received");
+        reject("Data not received");
     }
+});
+makeServerRequest.then(result => {
+    console.log(result);
 });
