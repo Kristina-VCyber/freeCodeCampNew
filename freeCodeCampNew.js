ca47cee071d04b5776543ab15c9614550fef1da8
Kristina-VCyber <hatkova@inbox.ru>
2022-02-15 22:53:27 +0300

    Ignore Case While Matching

--- freeCodeCampNew.js
+++ freeCodeCampNew.js
@@ -661,3 +661,8 @@ let result = waldoRegex.test(waldoIsHiding);// e this line
 let petString = "James has a pet cat.";
 let petRegex = /dog|cat|bird|fish/; // Change this line
 let result = petRegex.test(petString);
+
+//
+let myString = "freeCodeCamp";
+let fccRegex = /freeCodeCamp/i // Change this line
+let result = fccRegex.test(myString);
