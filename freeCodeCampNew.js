3ef37c88b04992384d2ae0ba5c8f6b3414ef828f
Kristina-VCyber <hatkova@inbox.ru>
2022-02-15 22:47:39 +0300

    Match a Literal String with Different Possibilities

--- freeCodeCampNew.js
+++ freeCodeCampNew.js
@@ -655,3 +655,9 @@
 let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
 let waldoRegex = /Waldo/; // Change this line
 let result = waldoRegex.test(waldoIsHiding);// e this line
+
+
+// Match a Literal String with Different Possibilities
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);
