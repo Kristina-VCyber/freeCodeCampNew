0776ca55f3d4ba8ff7f1680cdd3f088ea29211cc
Kristina-VCyber <hatkova@inbox.ru>
2022-02-17 21:51:53 +0300

    Restrict Possible Usernames

--- freeCodeCampNew.js
+++ freeCodeCampNew.js
@@ -778,3 +778,16 @@
 let movieName = "2001: A Space Odyssey";
 let noNumRegex = /\D/g; // Change this line
 let result = movieName.match(noNumRegex).length;
+
+// Restrict Possible Usernames
+
+// Usernames can only use alpha-numeric characters.
+//
+//     The only numbers in the username have to be at the end. There can be zero or more of them at the end. Username cannot start with the number.
+//
+//     Username letters can be lowercase and uppercase.
+
+    // Usernames have to be at least two characters long. A two-character username can only use alphabet letters as characters.
+let username = "JackOfAllTrades";
+let userCheck = /^[a-z][a-z]+\d*$|^[a-z]\d\d+$/ig;; // Change this line
+let result = userCheck.test(username);
