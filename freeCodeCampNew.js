85444929f022f13aa826bbdbe85d3ca1a43ce6e1
Kristina-VCyber <hatkova@inbox.ru>
2022-02-17 22:06:36 +0300

    Specify Upper and Lower Number of Matches

--- freeCodeCampNew.js
+++ freeCodeCampNew.js
@@ -807,3 +807,12 @@
 let sample = "Whitespace is important in separating words";
 let countNonWhiteSpace = /\S/g; // Change this line
 let result = sample.match(countNonWhiteSpace);
+
+
+
+// Specify Upper and Lower Number of Matches
+// Change the regex ohRegex to match the entire phrase Oh no only when it has 3 to 6 letter h's.
+let ohStr = "Ohhh no";
+
+let ohRegex = /Oh{3,6}\sno/; // Change this line
+let result = ohRegex.test(ohStr);
