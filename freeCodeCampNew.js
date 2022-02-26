b5e9a2dd93a78d3beb9e29655a52d56dcd4b8c9d
Kristina-VCyber <hatkova@inbox.ru>
2022-02-26 21:09:05 +0300

    Use Capture Groups to Search and Replace

--- freeCodeCampNew.js
+++ freeCodeCampNew.js
@@ -846,13 +846,20 @@
 // let result = pwRegex.test(sampleWord);
 
 // Check For Mixed Grouping of Characters
-let myString = "Eleanor Roosevelt";
-let myRegex = /(Franklin D.|Eleanor) Roosevelt/; // Change this line
-let result =  myRegex.test(myString); // Change this line
-// After passing the challenge experiment with myString and see how the grouping works
+// let myString = "Eleanor Roosevelt";
+// let myRegex = /(Franklin D.|Eleanor) Roosevelt/; // Change this line
+// let result =  myRegex.test(myString); // Change this line
+// // After passing the challenge experiment with myString and see how the grouping works
+//
+//
+// // Reuse Patterns Using Capture Groups
+// let repeatNum = "42 42 42";
+// let reRegex = /^(\d+)\s\1\s\1$/; // Change this line
+// let result = repeatNum.match(reRegex);
 
-
-// Reuse Patterns Using Capture Groups
-let repeatNum = "42 42 42";
-let reRegex = /^(\d+)\s\1\s\1$/; // Change this line
-let result = repeatNum.match(reRegex);
+// Use Capture Groups to Search and Replace
+let str = "one two three";
+let fixRegex = (/(\w+)\s(\w+)\s(\w+)/); // Change this line
+let replaceText = "$3 $2 $1"; // Change this line
+let result = str.replace(fixRegex, replaceText);
+console.log(result)
