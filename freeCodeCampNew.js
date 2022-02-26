4f3f00679654d58bf3dbb2ca71b2d1ff4d257ea3
Kristina-VCyber <hatkova@inbox.ru>
2022-02-27 00:22:37 +0300

    Remove Items Using splice

--- freeCodeCampNew.js
+++ freeCodeCampNew.js
@@ -1001,10 +1001,19 @@
 // console.log(mixedNumbers(['IV', 5, 'six']));
 
 // Remove Items from an Array with pop() and shift()
-function popShift(arr) {
-    let popped = arr.pop(); // Change this line
-    let shifted = arr.shift();; // Change this line
-    return [shifted, popped];
-}
+// function popShift(arr) {
+//     let popped = arr.pop(); // Change this line
+//     let shifted = arr.shift();; // Change this line
+//     return [shifted, popped];
+// }
+//
+// console.log(popShift(['challenge', 'is', 'not', 'complete']));
 
-console.log(popShift(['challenge', 'is', 'not', 'complete']));
+
+// Remove Items Using splice()
+
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// Only change code below this line
+arr.splice(1,4)
+// Only change code above this line
+console.log(arr);
