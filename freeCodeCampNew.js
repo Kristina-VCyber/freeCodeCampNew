047b5333520d1f63416db3dfd68f199fc99b05a3
Kristina-VCyber <hatkova@inbox.ru>
2022-02-26 22:13:19 +0300

    Understanding the Differences between the freeCodeCamp and Browser Console

--- freeCodeCampNew.js
+++ freeCodeCampNew.js
@@ -879,6 +879,10 @@ let b = 1;
 a++;
 // Only change code below this line
 
-
 let sumAB = a + b;
 console.log(a);
+
+// Understanding the Differences between the freeCodeCamp and Browser Console
+let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+console.log(output)
+console.clear()
