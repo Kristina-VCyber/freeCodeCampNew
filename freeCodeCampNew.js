0f3ef5720b0c2c4dd374d44ae20adeeb81a10023
Kristina-VCyber <hatkova@inbox.ru>
2022-02-27 00:05:36 +0300

    Use Caution When Reinitializing Variables Inside a Loop

--- freeCodeCampNew.js
+++ freeCodeCampNew.js
@@ -925,26 +925,51 @@
 // console.log(result);
 
 // Catch Missing Open and Closing Parenthesis After a Function Call
-function raiseToPower(b, e) {
-    return Math.pow(b, e);
-}
-
-let base = 2;
-let exp = 3;
-let power = raiseToPower(base, exp);
-console.log(power);
+// function raiseToPower(b, e) {
+//     return Math.pow(b, e);
+// }
+//
+// let base = 2;
+// let exp = 3;
+// let power = raiseToPower(base, exp);
+// console.log(power);
 
 
 // Catch Off By One Errors When Using Indexing
-function countToFive() {
-    let firstFive = "12345";
-    let len = firstFive.length;
-    // Only change code below this line
-    for (let i = 0; i < len; i++) {
-        // Only change code above this line
-        console.log(firstFive[i]);
+// function countToFive() {
+//     let firstFive = "12345";
+//     let len = firstFive.length;
+//     // Only change code below this line
+//     for (let i = 0; i < len; i++) {
+//         // Only change code above this line
+//         console.log(firstFive[i]);
+//     }
+// }
+//
+// countToFive();
+
+
+// Use Caution When Reinitializing Variables Inside a Loop
+function zeroArray(m, n) {
+    // Creates a 2-D array with m rows and n columns of zeroes
+    let newArray = [];
+
+    for (let i = 0; i < m; i++) {
+        // Adds the m-th row into newArray
+        let row = [];
+        for (let j = 0; j < n ; j++) {
+            // Pushes n zeroes into the current row to create the columns
+            row.push(0);
+        }
+        // Pushes the current row, which now has n zeroes in it, to the array
+        newArray.push(row);
     }
+    return newArray;
 }
 
-countToFive();
+let matrix = zeroArray(3, 2);
+console.log(matrix);
+
+
+
 
