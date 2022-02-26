c05e6fcde91a656e5f227e8d8a3e6c2e9e70c526
Kristina-VCyber <hatkova@inbox.ru>
2022-02-26 22:19:03 +0300

    Use typeof to Check the Typeof a Variable

--- freeCodeCampNew.js
+++ freeCodeCampNew.js
@@ -874,15 +874,24 @@
 
 // Use the JavaScript Console to Check the Value of a Variable
 
-let a = 5;
-let b = 1;
-a++;
+// let a = 5;
+// let b = 1;
+// a++;
 // Only change code below this line
 
-let sumAB = a + b;
-console.log(a);
+// let sumAB = a + b;
+// console.log(a);
+//
+// // Understanding the Differences between the freeCodeCamp and Browser Console
+// let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+// console.log(output)
+// console.clear()
 
-// Understanding the Differences between the freeCodeCamp and Browser Console
-let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
-console.log(output)
-console.clear()
+// Use typeof to Check the Type of a Variable
+
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Only change code below this line
+console.log(typeof seven);
+console.log(typeof three);
