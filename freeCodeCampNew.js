256f691deb41976627f950ce3fc812b7f24d1d41
Kristina-VCyber <hatkova@inbox.ru>
2022-02-27 00:14:39 +0300

    Add Items to an Array with push and unshift

--- freeCodeCampNew.js
+++ freeCodeCampNew.js
@@ -978,15 +978,27 @@
 // }
 
 // Use an Arr to Store a Collection of Data
-let yourArray = [1,"hi",true,3,"world"]; // Change this line
+// let yourArray = [1,"hi",true,3,"world"]; // Change this line
 
 
 // Access an Arr's Contents Using Bracket Notation
-let myArray = ["a", "b", "c", "d"];
-// Only change code below this line
-myArray[1] = "Tina";
-// Only change code above this line
-console.log(myArray);
+// let myArray = ["a", "b", "c", "d"];
+// // Only change code below this line
+// myArray[1] = "Tina";
+// // Only change code above this line
+// console.log(myArray);
+//
+//
+
+// Add Items to an Array with push() and unshift()
+function mixedNumbers(arr) {
+    // Only change code below this line
+    arr.unshift('I', 2, 'three');
+    arr.push(7, 'VIII', 9);
+    // Only change code above this line
+    return arr;
+}
+console.log(mixedNumbers(['IV', 5, 'six']));
 
 
 
