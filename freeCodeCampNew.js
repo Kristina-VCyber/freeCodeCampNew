2a839237856330766b7398d37be0b077c69a822b
Kristina-VCyber <hatkova@inbox.ru>
2022-02-27 00:02:22 +0300

    Catch Mixed Usage of Single and Double Quotes

--- freeCodeCampNew.js
+++ freeCodeCampNew.js
@@ -901,8 +901,12 @@
 // let netWorkingCapital = receivables - payables;
 // console.log(`Net working capital is: ${netWorkingCapital}`);
 // Catch Unclosed Parentheses, Brackets, Braces and Quotes
-let myArray = [1, 2, 3];
-let arraySum = myArray.reduce(function (previousValue, currentValue) {
-    return previousValue + currentValue
-}, 0)
-console.log(`Sum of array values is: ${arraySum}`);
+// let myArray = [1, 2, 3];
+// let arraySum = myArray.reduce(function (previousValue, currentValue) {
+//     return previousValue + currentValue
+// }, 0)
+// console.log(`Sum of array values is: ${arraySum}`);
+
+// Catch Mixed Usage of Single and Double Quotes
+let innerHtml = "<p>Click here to <a href='#Home'>return home</a></p>";
+console.log(innerHtml);
