dc1cbd6d1f5a4f1bf3b8b2f27fb59be11284d67c
Kristina-VCyber <hatkova@inbox.ru>
2022-02-27 16:35:56 +0300

    Combine Arrays with the Spread Operator

--- freeCodeCampNew.js
+++ freeCodeCampNew.js
@@ -1043,15 +1043,25 @@
 
 //
 // Copy an Array with the Spread Operator
-function copyMachine(arr, num) {
-    let newArr = [];
-    while (num >= 1) {
-        // Only change code below this line
-        newArr.push([...arr]);
-        // Only change code above this line
-        num--;
-    }
-    return newArr;
+// function copyMachine(arr, num) {
+//     let newArr = [];
+//     while (num >= 1) {
+//         // Only change code below this line
+//         newArr.push([...arr]);
+//         // Only change code above this line
+//         num--;
+//     }
+//     return newArr;
+// }
+//
+// console.log(copyMachine([true, false, true], 2));
+
+
+// Combine Arrays with the Spread Operator
+function spreadOut() {
+    let fragment = ['to', 'code'];
+    let sentence = ["learning", ...fragment,"is", "fun"]; // Change this line
+    return sentence;
 }
 
-console.log(copyMachine([true, false, true], 2));
+console.log(spreadOut());
