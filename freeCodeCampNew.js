3fdc65baa45f3b8ef6d06e4cea260243045104e8
Kristina-VCyber <hatkova@inbox.ru>
2022-02-27 17:21:09 +0300

    Create complex multi-dimensional arrays

--- freeCodeCampNew.js
+++ freeCodeCampNew.js
@@ -1067,30 +1067,46 @@
 // console.log(spreadOut());
 
 // Check For The Presence of an Element With indexOf
-function quickCheck(arr, elem) {
-    // Only change code below this line
-    if (arr.indexOf(elem) >= 0) {
-        return true;
-    }
-    return false;
-}
-// Only change code above this line
-
-
-console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+// function quickCheck(arr, elem) {
+//     // Only change code below this line
+//     if (arr.indexOf(elem) >= 0) {
+//         return true;
+//     }
+//     return false;
+// }
+// // Only change code above this line
+//
+//
+// console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
 
 
 // Iterate Through All an Array's Items Using For Loops
-function filteredArray(arr, elem) {
-    let newArr = [];
-    // change code below this line
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i].indexOf(elem) == -1) {
-            newArr.push(arr[i]);
-        }
-    }
-    // change code above this line
-    return newArr;
-}
+// function filteredArray(arr, elem) {
+//     let newArr = [];
+//     // change code below this line
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i].indexOf(elem) == -1) {
+//             newArr.push(arr[i]);
+//         }
+//     }
+//     // change code above this line
+//     return newArr;
+// }
+//
+// console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
 
-console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
+// Create complex multi-dimensional arrays
+
+let myNestedArray = [
+    // Only change code below this line
+    [1,"hi",false],
+    [1,"hi",false],
+    [1,"hi",false, ["deep"]],
+    [1,"hi",false,[["deeper"]]],
+    [1,"hi",false,[[["deepest"]]]]
+
+    // change code above this line
+];
+
+// Only change code above this line
+
